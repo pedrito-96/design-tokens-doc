cf9eb0cafcddd63043c2eac9bfedee8aa4807dfe
Pietro Stivanello <68153217+pedrito-96@users.noreply.github.com>
2025-02-07 15:20:46 +0100

    feat: doc structure

--- components/Logo.vue
+++ components/Logo.vue
@@ -3,3 +3,10 @@
     <img width="120" src="/public/logo.svg" />
   </template>
 </template>
+<style lang="css">
+@media (prefers-color-scheme: dark) {
+  img {
+    filter: invert(1);
+  }
+}
+</style>
